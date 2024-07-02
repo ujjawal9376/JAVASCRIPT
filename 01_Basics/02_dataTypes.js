6dec3415c5564d0dc4934807331f35a608b41ed4
Ujjawal Kachawal <151642573+ujjawal9376@users.noreply.github.com>
2024-07-02 11:15:55 +0530

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict";  // treat all js code as newer version 
+
+// alert("Ujjawal")  // We are using nodejs, not browser
+
+//Code Readability  should be high
+
+// console.log(5+4); console.log("Ujjawal");  // Please don't Write Code Like This
+
+// Right Way to Write a code
+// console.log(10+10);
+// console.log("Ujjawal Sharma");
+
+// dataTypes 
