9ee8cc2b274b62a3524972b56dc4e202f538e420
Ujjawal Kachawal <151642573+ujjawal9376@users.noreply.github.com>
2024-07-02 05:54:34 +0000

    dataTypes

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -10,4 +10,18 @@
 // console.log(10+10);
 // console.log("Ujjawal Sharma");
 
-// dataTypes 
+
+// DATATYPES
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
