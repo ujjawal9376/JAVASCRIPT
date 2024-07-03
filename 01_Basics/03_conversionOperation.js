ef2ec90d577534b813694ab9ff55751638f12c07
Ujjawal Kachawal <151642573+ujjawal9376@users.noreply.github.com>
2024-07-03 09:20:25 +0000

    Conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = 100;
+// console.log(typeof score); // Number
+// console.log(typeof (score)); // Another way to write typeof by (method)
+
+let valueInString = String(score);  // Change the type from number to string
+// console.log(typeof valueInString);  // string
+// console.log(valueInString);  // score ki value change nhi hogi ONLY TYPE change hoga
+
+// "50" => 50
+// "55dkd" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true;
+// 0 => false
+// "" => false
+// "Ujjawal" => true
+
+let someNumber = 54;
+let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
